b24929ddbeb131bf0ea08a493e0a936778059981
imran <imranloveh01@gmail.com>
2024-03-07 23:55:14 +0500

    Refactor Latest component styles

--- components/home/Latest.tsx
+++ components/home/Latest.tsx
@@ -8,36 +8,36 @@ const Latest = async () => {
 
   return (
     <>
-      <div className="w-full min-h-screen flex flex-col items-center">
-        <div className="self-start container">
-          <h1 className="text-white mt-10 text-2xl font-bold">MiniSplash</h1>
+      <div className="flex min-h-screen w-full flex-col items-center">
+        <div className="container self-start">
+          <h1 className="mt-10 text-2xl font-bold text-white">MiniSplash</h1>
           <p>Explore the Free Photos from Unsplash</p>
         </div>
-        <div className="columns-2 md:columns-3 max-w-screen-xl bg-white rounded w-full my-16 p-5 gap-3">
+        <div className="my-16 w-full max-w-screen-xl columns-2 gap-3 rounded bg-white p-5 md:columns-3">
           {photos.map((item) => (
             <div
               key={item.id}
-              className="break-inside-avoid relative overflow-hidden rounded mb-4"
+              className="relative mb-4 break-inside-avoid overflow-hidden rounded"
             >
-              <div className="relative group">
+              <div className="group relative">
                 <DownloadButton
-                  className="hidden group-hover:block z-10"
+                  className="z-10 hidden group-hover:block"
                   imgTitle={item.alt_description}
                   imageUrl={item.urls.full}
                 />
-                <div className="absolute left-3 bottom-5 z-10">
+                <div className="absolute bottom-5 left-3 z-10">
                   <div className="flex items-center">
                     <Image
                       src={item.user.profile_image.large}
                       alt={item.user.name}
                       width={100}
                       height={100}
-                      className="rounded-full object-cover object-center w-10 h-auto"
+                      className="h-auto w-10 rounded-full object-cover object-center"
                     />
-                    <div className="flex-1 ml-2">
+                    <div className="ml-2 flex-1">
                       <Link
                         href={`users/${item.user.username}`}
-                        className="text-base hover:text-white text-white/70"
+                        className="text-base text-white/70 hover:text-white"
                       >
                         {item.user.name}
                       </Link>
@@ -45,7 +45,7 @@ const Latest = async () => {
                   </div>
                 </div>
                 <Link href={`/photo/${item.id}`}>
-                  <div className="absolute inset-0 group-hover:bg-slate-900/40 cursor-pointer transition-all"></div>
+                  <div className="absolute inset-0 cursor-pointer transition-all group-hover:bg-slate-900/40"></div>
                   <Image
                     src={item.urls.small}
                     alt={item.id}
@@ -53,7 +53,7 @@ const Latest = async () => {
                     height={400}
                     placeholder="blur"
                     blurDataURL={item.urls.small}
-                    className="rounded cursor-pointer w-full h-full object-cover"
+                    className="h-full w-full cursor-pointer rounded object-cover"
                   />
                 </Link>
               </div>
